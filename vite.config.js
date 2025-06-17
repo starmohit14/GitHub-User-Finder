e9f1425c17dca5a6e0bd8defd3ea59e232fc9e84
Mohit Sharma <starmohit14@gmail.com>
2025-06-17 17:26:59 +0530

    added first code

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GitHub-User-Finder/', // 👈 must match your repo name exactly
+  plugins: [react()],
 })
+
+
+
